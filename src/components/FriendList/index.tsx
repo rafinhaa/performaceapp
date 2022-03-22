e1a13abec954524d43be649aa7a93ac3dd3bb9d0
Rafael Soncine <rafinhaa@me.com>
2022-03-22 11:09:11 -0300

    Chapter V - Otimizações - useMemo

--- src/components/FriendList/index.tsx
+++ src/components/FriendList/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useMemo } from "react";
+import { View, Text } from "react-native";
 import { ISearchResultProps } from "../../types";
 import { Friend } from "../Friend";
 
 const FriendList: React.FC<ISearchResultProps> = ({ data }) => {
+  const totalLikes = useMemo(() => {
+    return data.reduce((likes, friend) => {
+      return likes + friend.likes;
+    }, 0);
+  }, [data]);
+
   return (
     <View>
+      <Text>{`Total de likes: ${totalLikes}`}</Text>
       {data.map((data) => (
         <View>
           <Friend key={String(data.id)} friend={data} />
